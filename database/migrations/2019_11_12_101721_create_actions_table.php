0ff571d10a57dc7a27e8a10078ffdcdca30e2f55
Laurens06 <43202196+Laurens06@users.noreply.github.com>
2019-11-12 11:44:13 +0100

    Add Actions model, migrations and seeds

---
+++ database/migrations/2019_11_12_101721_create_actions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->text('samengezond_url');
+            $table->integer('points_required');
+            $table->double('old_price');
+            $table->string('discount');
+            $table->double('new_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actions');
+    }
+}
