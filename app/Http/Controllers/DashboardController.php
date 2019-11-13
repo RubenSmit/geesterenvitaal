ec5104c006dd4fa3a10e4f622ee740513d738478
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-11-13 13:47:51 +0100

    Fix linting

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,6 @@ use function Psy\debug;
 
 class DashboardController extends Controller
 {
-
     public function index()
     {
         return view('index', ['challenges' => Challenge::inRandomOrder()->take(3)->get(), 'activities' => Activity::upcoming()->take(3)->get()]);
