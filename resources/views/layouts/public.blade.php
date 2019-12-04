2210a75e6944926f59e9dfc425c3f801cc3e64a9
Ruben Smit <RubenSmit@users.noreply.github.com>
2019-12-04 15:56:05 +0100

    Add active page highlighting

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,11 +7,11 @@
         <img class="nav-brand" src="{{url('img/logo.bmp')}}" alt="Geesteren Vitaal - Home"/>
     </a>
     <div class="nav-menu">
-        <a class="nav-item" href="{{url('/activiteit')}}">activiteiten</a>
-        <a class="nav-item" href="{{url('/actie')}}">spaaracties</a>
-        <a class="nav-item" href="{{url('/uitdaging')}}">uitdagingen</a>
+        <a class="nav-item {{(Request::is('activiteit') || Request::is('activiteit/*')) ? "active": ""}}" href="{{url('/activiteit')}}">activiteiten</a>
+        <a class="nav-item {{(Request::is('actie') || Request::is('actie/*')) ? "active": ""}}" href="{{url('/actie')}}">spaaracties</a>
+        <a class="nav-item {{(Request::is('uitdaging') || Request::is('uitdaging/*')) ? "active": ""}}" href="{{url('/uitdaging')}}">uitdagingen</a>
         @foreach($site_settings["nav_items"] as $item)
-            <a class="nav-item" href="{{url('/pagina/'.$item->id)}}">{{$item->title}}</a>
+            <a class="nav-item {{(Request::is('pagina/'.$item->id)) ? "active": ""}}" href="{{url('/pagina/'.$item->id)}}">{{$item->title}}</a>
         @endforeach
         @guest
             <a class="nav-item nav-pill" href="https://www.samengezond.nl" target="_blank"
